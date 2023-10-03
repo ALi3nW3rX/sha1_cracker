4a6f82f7b36b18c0d6546d26ed4d0f5ebb62df20
ali3nw3rx <alienwurx@gmail.com>
2023-10-03 10:35:13 -0700

    initial commit

---
+++ sha1cracker/target/debug/deps/sha1cracker-1b9506370c85d995.d
@@ -0,0 +1,5 @@
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\deps\sha1cracker-1b9506370c85d995.rmeta: src\main.rs
+
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\deps\sha1cracker-1b9506370c85d995.d: src\main.rs
+
+src\main.rs:
---
+++ sha1cracker/target/debug/deps/sha1cracker-83beaaf948360b99.d
@@ -0,0 +1,5 @@
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\deps\sha1cracker-83beaaf948360b99.rmeta: src\main.rs
+
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\deps\sha1cracker-83beaaf948360b99.d: src\main.rs
+
+src\main.rs:
