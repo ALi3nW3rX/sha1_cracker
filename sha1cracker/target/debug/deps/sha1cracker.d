7f58713ae34300abfedf6a108bfac76664e92b44
ali3nw3rx <alienwurx@gmail.com>
2023-10-03 10:34:58 -0700

    hello world

---
+++ sha1cracker/target/debug/deps/sha1cracker.d
@@ -0,0 +1,5 @@
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\deps\sha1cracker.exe: src\main.rs
+
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\deps\sha1cracker.d: src\main.rs
+
+src\main.rs:
---
+++ sha1cracker/target/debug/sha1cracker.d
@@ -0,0 +1,1 @@
+C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\target\debug\sha1cracker.exe: C:\Users\mr3ll10t\Desktop\Projects\sha1_cracker\sha1cracker\src\main.rs
